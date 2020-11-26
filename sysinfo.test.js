ed543acfd5a02bd41e1d82bb1497988301b3d6af
mahdi <g4malo@gmail.com>
2020-11-25 21:35:52 -0500

    updated test

--- sysinfo.test.js
+++ sysinfo.test.js
@@ -3,5 +3,5 @@ const systemInfo = require("./sysinfo");
 
 test("sysinfo test", () => {
   const result = systemInfo();
-  expect(result.Total_memory_GB).toBe(14);
+  expect(result.Total_memory_GB).toBe(15);
 });
